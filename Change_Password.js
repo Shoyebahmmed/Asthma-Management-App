02c8a4ef8d8a6822991aa427282c60fa748f04d0
Ujjwal314 <101781291+Ujjwal314@users.noreply.github.com>
2023-04-20 11:06:21 +1000

    Update Change_Password.js

--- Change_Password.js
+++ Change_Password.js
@@ -1,80 +1,78 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Image, Switch, TextInput, Button, TouchableOpacity, TouchableWithoutFeedback, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity, TouchableWithoutFeedback, ImageBackground } from 'react-native';
 import Head from './head';
 import NavBar from './navBar';
-import { isEnabled } from 'react-native/Libraries/Performance/Systrace';
 
-const Notification_Setting = () => {
+const Change_Password = () => {
 
-    const [showNotification, setshowNotification] = React.useState(false);
-    const [emailNotification, setemailNotification] = React.useState(false);
-    const [allowSound, setallowSound] = React.useState(false);
-    const [allowVibration, setallowVibration] = React.useState(false);
-
+    const [current, setCurrent] = useState(true);
+    const [newPassword, setnewPassword] = useState(true);
+    const [confirm, setconfirm] = useState(true);
+    const [buttonPoss, setButtonPoss] = useState(true);
 
 
     return (
         <ImageBackground source = {require('./img_and_icon/drawable-hdpi/Rectangle.png')} style={styles.container}>
             <Head /> 
             <View style={styles.header}>
-                <Image source={require('./img_and_icon/drawable-hdpi/NotificationSetting.png')} />
+                {/* <TouchableOpacity onPress={() => alert('going back')}
+                    activeOpacity={0.5}>
+                    <Image source={require('./img_and_icon/drawable-hdpi/IconArrowLeft.png')} 
+                    style={styles.headerImage} 
+                    resizeMode='stretch'
+                    />
+                </TouchableOpacity> */}
+                <Image source={require('./img_and_icon/drawable-hdpi/ChangePassword.png')} />
             </View>
 
             <View style={styles.body}>
-                <View style={styles.first}>
-                    <Image source={require('./img_and_icon/drawable-hdpi/ShowNotification.png')} />
-                    {/* <Text>{isEnabled.toString()}</Text> */}
-                    <Switch 
-                        style={styles.switch}
-                        value={showNotification}
-                        onValueChange={(value) => setshowNotification(value)}
-                        trackColor={{ false: "#C5D0D8", true: "#2699FB"}}
-                        thumbColor={showNotification ? "#FFFFFF" : "#2699FB"}
-                    
-                        ios_backgroundColor="2699FB"     //for ios         
+                
+                <View style={styles.current}>
+                    <Image source={require('./img_and_icon/drawable-hdpi/LockIcon.png')} style={styles.bodyImage} />
+                    <TextInput
+                    style={styles.textBox}
+                    onChangeText={(inputText) => setText(inputText)}
+                    value={current}
+                    placeholder="Current Password"
                     />
-                    {/* <Switch 
-                        style={styles.switch}
-                        value={isEnabled}
-                        onValueChange={(value) => setIsEnabled(value)}
-                        trackColor={{ false: "#FFFFFF", true: "#2699FB"}}
-                        thumbColor={isEnabled ? "#FFFFFF" : "#2699FB"}
-                        ios_backgroundColor="2699FB"     //for ios         
+                </View>
+                <View style={styles.new}>
+                    <Image source={require('./img_and_icon/drawable-hdpi/LockIcon.png')} style={styles.bodyImage} />
+                    <TextInput
+                    style={styles.textBox}
+                    onChangeText={(inputText) => setText(inputText)}
+                    value={newPassword}
+                    placeholder="New Password"
+                    />
+                    {/* <Image style={styles.line} source={require('./img_and_icon/drawable-hdpi/Line.png')} /> */}
+
+                </View>
+                <View style={styles.confirm}>
+                    <Image source={require('./img_and_icon/drawable-hdpi/LockIcon.png')} style={styles.bodyImage} />
+                    <TextInput
+                    style={styles.textBox}
+                    onChangeText={(inputText) => setText(inputText)}
+                    value={confirm}
+                    placeholder="Confirm Password"
+                    />
+                </View>
+
+                <View style={styles.button}>
+                    <TouchableOpacity onPress={() => alert('password is changed')}
+                        activeOpacity={0.5}>
+                        <Image source={require('./img_and_icon/drawable-hdpi/ButtonChangePassword.png')} 
+                        style={styles.buttonImage} 
+                        resizeMode='stretch'
+                        />
+                    </TouchableOpacity>
+                    {/* <Button
+                        title="Change Password"
+                        onPress={() => {
+
+                        }}
                     /> */}
                 </View>
-                <View style={styles.second}>
-                    <Image source={require('./img_and_icon/drawable-hdpi/EmailNotification.png')} />
-                    <Switch 
-                        style={styles.switch}
-                        value={emailNotification}
-                        onValueChange={(value) => setemailNotification(value)}
-                        trackColor={{ false: "#C5D0D8", true: "#2699FB"}}
-                        thumbColor={emailNotification ? "#FFFFFF" : "#2699FB"}
-                        ios_backgroundColor="2699FB"     //for ios         
-                    />
-                </View>
-                <View style={styles.third}>
-                    <Image source={require('./img_and_icon/drawable-hdpi/AllowSound.png')} />
-                    <Switch 
-                        style={styles.switch}
-                        value={allowSound}
-                        onValueChange={(value) => setallowSound(value)}
-                        trackColor={{ false: "#C5D0D8", true: "#2699FB"}}
-                        thumbColor={allowSound ? "#FFFFFF" : "#2699FB"}
-                        ios_backgroundColor="2699FB"     //for ios         
-                    />
-                </View>
-                <View style={styles.fourth}>
-                    <Image source={require('./img_and_icon/drawable-hdpi/AllowVibration.png')} />
-                    <Switch 
-                        style={styles.switch}
-                        value={allowVibration}
-                        onValueChange={(value) => setallowVibration(value)}
-                        trackColor={{ false: "#C5D0D8", true: "#2699FB"}}
-                        thumbColor={allowVibration ? "#FFFFFF" : "#2699FB"}
-                        ios_backgroundColor="2699FB"     //for ios         
-                    />
-                </View>
+    
             </View>
             <NavBar /> 
         </ImageBackground>
@@ -90,7 +88,7 @@ const styles = StyleSheet.create({
     header: {
         height: 100,
         fontSize: 24,
-        marginTop: 30,
+        marginTop: 90,
         marginLeft: 60,
         flexDirection: 'row',
     },
@@ -102,36 +100,42 @@ const styles = StyleSheet.create({
     },
     
     body: {
-        marginLeft: 30,
+        marginLeft: 40,
     },
 
-    first: {
-        marginBottom: 20,
+    current: {
+        marginTop: 10,
         flexDirection: 'row',
-    },
-
-    second: {
         marginBottom: 20,
-        flexDirection: 'row',
+        marginLeft: 10,
     },
-
-    third: {
+    new: {
+        flexDirection: 'row',
         marginBottom: 20,
-        flexDirection: 'row',
+        marginLeft: 10,
     },
-
-    fourth: {
+    confirm: {
+        flexDirection: 'row',
         marginBottom: 20,
-        flexDirection: 'row',
+        marginLeft: 10,
     },
-
-    switch: {
-        marginHorizontal: -70
+    bodyImage: {
+        width: 26,
+        height: 26,
+        marginHorizontal: 10,
     },
+    // line: {
+    //     alignItems: 'center',
+    //     marginTop: 20,
+    //     marginRight: 100,
+    // },
+    button: {
+        marginTop: 100,
+        marginRight: 45,
+        alignItems: 'center',
+    },
+    buttonImage: {
+    }
   });
 
-export default Notification_Setting;
-
-
-                    
-                
+export default Change_Password;
